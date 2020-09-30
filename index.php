2d1295a483d73a2574197eed9377b7e789062071
santiagoG0024 <67969616+santiagoG0024@users.noreply.github.com>
2020-09-30 14:46:16 -0500

    se creo el formulario

--- index.php
+++ index.php
@@ -9,9 +9,28 @@
 <body>
         
     <header>
+        <h1>TIENDA.COM</h1>
+        <hr>
     </header>  
 
     <main>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+   
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
     </main>
 
     <footer>
